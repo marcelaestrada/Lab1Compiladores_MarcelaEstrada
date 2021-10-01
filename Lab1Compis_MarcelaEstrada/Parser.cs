e8405fe07b82900f27b61e9979582557806019cd
Marcela Estrada <marcelaestradarl@gmail.com>
2021-10-01 13:22:52 -0600

    Parser ready

--- Lab1Compis_MarcelaEstrada/Parser.cs
+++ Lab1Compis_MarcelaEstrada/Parser.cs
@@ -6,5 +6,135 @@ namespace Lab1Compis_MarcelaEstrada
 {
     class Parser
     {
+        Scanner _scanner;
+        Token _token;
+
+        private void E()
+        {
+            switch (_token.Tag)
+            {
+                case TokenType.LParen:
+                case TokenType.Symbol:
+                case TokenType.Substract:
+                    T();
+                    EP();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void EP()
+        {
+            switch (_token.Tag)
+            {
+                case TokenType.Add:
+                    Match(TokenType.Add);
+                    T();
+                    EP();
+                    break;
+
+                case TokenType.Substract:
+                    Match(TokenType.Substract);
+                    T();
+                    EP();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void T()
+        {
+            switch (_token.Tag)
+            {
+                case TokenType.LParen:
+                case TokenType.Symbol:
+                case TokenType.Substract:
+                    F();
+                    TP();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void TP()
+        {
+            switch (_token.Tag)
+            {
+                case TokenType.Multiply:
+                    Match(TokenType.Multiply);
+                    F();
+                    TP();
+                    break;
+
+                case TokenType.Divide:
+                    Match(TokenType.Divide);
+                    F();
+                    TP();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void F()
+        {
+            switch (_token.Tag)
+            {
+                case TokenType.Substract:
+                    Match(TokenType.Substract);
+                    F();
+                    break;
+
+                case TokenType.Symbol:
+                    Match(TokenType.Symbol);
+                    F();
+                    break;
+
+                case TokenType.LParen:
+                    Match(TokenType.LParen);
+                    E();
+                    Match(TokenType.RParen);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void Match(TokenType tag)
+        {
+            if(_token.Tag == tag)
+            {
+                _token = _scanner.GetToken();
+            }
+            else
+            {
+                throw new Exception("Error de sintaxis");
+            }
+        }
+
+        public void Parse(string regexp)
+        {
+            _scanner = new Scanner(regexp + (char)TokenType.EOF);
+            _token = _scanner.GetToken();
+            switch (_token.Tag)
+            {
+                case TokenType.LParen:
+                case TokenType.Symbol:
+                    E();
+                    break;
+
+                default:
+                    break;
+            }
+            Match(TokenType.EOF);
+        }
     }
 }
--- Lab1Compis_MarcelaEstrada/Program.cs
+++ Lab1Compis_MarcelaEstrada/Program.cs
@@ -8,21 +8,28 @@ namespace Lab1Compis_MarcelaEstrada
         static void Main(string[] args)
         {
 
-            Console.WriteLine("LABORATORIO 1\nIngrese la expresion algebraica que se desea resolver");
-            string regex = Console.ReadLine();
-            Scanner scanner = new Scanner(regex);
-            Token nextToken;
+            //Console.WriteLine("LABORATORIO 1\nIngrese la expresion algebraica que se desea resolver");
+            //string regex = Console.ReadLine();
+            //Scanner scanner = new Scanner(regex);
+            //Token nextToken;
 
-            do
-            {
-                nextToken = scanner.GetToken();
-                Console.WriteLine("Token: {0}\nValor: {1} \n", nextToken.Tag, nextToken.Value);
-                if(nextToken.banderaRParen != nextToken.banderaLParen)
-                {
-                    throw new Exception("Lex error");
-                }
-            } while (nextToken.Tag != TokenType.EOF);
-            
+            ////do
+            ////{
+            ////    nextToken = scanner.GetToken();
+            ////    Console.WriteLine("Token: {0}\nValor: {1} \n", nextToken.Tag, nextToken.Value);
+            ////    if(nextToken.banderaRParen != nextToken.banderaLParen)
+            ////    {
+            ////        throw new Exception("Lex error");
+            ////    }
+            ////} while (nextToken.Tag != TokenType.EOF);
+            ///
+
+            string regexp = Console.ReadLine();
+            Parser parser = new Parser();
+            parser.Parse(regexp);
+            Console.WriteLine("Expresión OK");
+            Console.ReadLine();
+
         }
     }
 }
